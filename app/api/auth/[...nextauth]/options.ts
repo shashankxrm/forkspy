46dd850b58fec3510be56cd62e4373b5a1666d77
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-01-28 23:47:22 +0530

    fixed build error in options about session interface

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,11 @@
-import type {NextAuthOptions} from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { Session } from "next-auth";
+
+// Extend the default Session type
+interface CustomSession extends Session {
+  accessToken?: string;
+}
 
 const clientId = process.env.GITHUB_ID;
 const clientSecret = process.env.GITHUB_SECRET;
@@ -9,19 +15,19 @@ if (!clientId || !clientSecret) {
 }
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: clientId,
-            clientSecret: clientSecret,
-            authorization: {
-              params: {
-                prompt: 'select_account',
-              }
-            }
-        })
-    ],
-    pages: {
-    signIn: '/auth/signin', // Specify the custom sign-in page
+  providers: [
+    GithubProvider({
+      clientId: clientId,
+      clientSecret: clientSecret,
+      authorization: {
+        params: {
+          prompt: 'select_account',
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '/auth/signin',
   },
   callbacks: {
     async jwt({ token, account }) {
@@ -30,10 +36,11 @@ export const authOptions: NextAuthOptions = {
       }
       return token
     },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken
-      return session
+    async session({ session, token }): Promise<CustomSession> {
+      return {
+        ...session,
+        accessToken: token.accessToken as string
+      }
     },
   },
-}; 
-   
+};
