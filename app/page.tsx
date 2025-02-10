cefdbdc0ecece504871ca5867b6f7dd0e62b6dfe
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-02-10 17:26:58 +0530

    sorted tracked repos by date added

--- app/page.tsx
+++ app/page.tsx
@@ -159,14 +159,17 @@ export default function Dashboard() {
 
       <h2 className="text-xl font-bold mb-2">Tracked Repositories</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {repositories.map((repo) => (
-          <GitHubRepoCard
-        key={repo._id}
-        repo={repo}
-        onTrackToggle={handleTrackToggle}
-        isTracked={true}
-          />
-        ))}
+        {repositories
+          .slice()
+          .reverse()
+          .map((repo) => (
+            <GitHubRepoCard
+              key={repo._id}
+              repo={repo}
+              onTrackToggle={handleTrackToggle}
+              isTracked={true}
+            />
+          ))}
       </div>
     </div>
   );
